cd0652c32f09a189445cc38c2e66dec4bd9c7cc2
Tomás Matos <66218733+6kz@users.noreply.github.com>
2026-04-13 15:18:56 +0100

    Projeto finalizado e completo para entrega

--- noticias/views.py
+++ noticias/views.py
@@ -1,17 +1,25 @@
-# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Artigo, Comentario
 from .forms import ComentarioForm
 
+# ESTA É A FUNÇÃO QUE ESTÁ A FALTAR:
+def home(request):
+    artigos = Artigo.objects.all()
+    return render(request, 'noticias/home.html', {'artigos': artigos})
+
+def detalhe_artigo(request, artigo_id):
+    artigo = get_object_or_404(Artigo, id=artigo_id)
+    return render(request, 'noticias/artigo.html', {'artigo': artigo})
+
 def pagina_comentarios(request, artigo_id):
     artigo = get_object_or_404(Artigo, id=artigo_id)
-    comentarios = artigo.comentarios.all() # Vai buscar todos os comentários deste artigo
+    comentarios = artigo.comentarios.all()
 
     if request.method == 'POST':
         form = ComentarioForm(request.POST)
         if form.is_valid():
             comentario = form.save(commit=False)
-            comentario.artigo = artigo # Associa o comentário ao artigo atual
+            comentario.artigo = artigo
             comentario.save()
             return redirect('pagina_comentarios', artigo_id=artigo.id)
     else:
