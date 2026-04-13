6482c79872a371401e685d0fee739a4646c39a91
Tomás Matos <66218733+6kz@users.noreply.github.com>
2026-04-13 15:04:59 +0100

    Implementação da listagem e detalhe de artigos

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Artigo, Comentario
+
+admin.site.register(Artigo)
+
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('artigo', 'texto', 'data_criacao')
+
+admin.site.register(Comentario, ComentarioAdmin)
